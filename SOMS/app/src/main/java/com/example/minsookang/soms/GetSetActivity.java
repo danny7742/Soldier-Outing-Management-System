f8f597463bc60e9d9ed8d679e0c476a2e5733e34
sungjae <alstjdwo1601@naver.com>
2019-05-11 23:43:10 +0900

    MapsActivity
    각종 gradle, xml 수정
    맵띄움

--- SOMS/app/src/main/java/com/example/minsookang/soms/GetSetActivity.java
+++ SOMS/app/src/main/java/com/example/minsookang/soms/GetSetActivity.java
@@ -1,12 +1,59 @@
 package com.example.minsookang.soms;
 
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.w3c.dom.Text;
+
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
 
-public class GetSetActivity implements Serializable{
+import com.google.firebase.firestore.FirebaseFirestore;
+import static android.support.constraint.Constraints.TAG;
+
+
+public class GetSetActivity implements Serializable {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    final DocumentReference docRef = db.collection("UserInformation").document("UserInfo");
+
 
     private String UserName = "";
     private String Password = "";
@@ -27,11 +74,28 @@ public class GetSetActivity implements Serializable{
     private SimpleDateFormat OutingStartDate = new SimpleDateFormat("yyyy.MM.dd HH:mm");
     private SimpleDateFormat OutingArriveDate = new SimpleDateFormat("yyyy.MM.dd HH:mm");
 
+
     public GetSetActivity(String UserName){
         this.UserName = UserName;
     }
 
-    String getUserName(){ return UserName; }
+    String getUserName(){
+        db.collection("UserInformation")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG,"이름 " + document.getString("UserName"));
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        return UserName;
+    }
 
     String getPassword(){ return Password; }
 
